abb478d3382edc7323c518c55171fa6676f1c6ca
Anuj Singh <anujk920@gmail.com>
2020-09-10 01:09:22 +0530

    Changes As per Heroku Deployment

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ const app = express();
 
 // app.use(forceSSL());
 
-app.use(express.static(__dirname + '/dist/Email-Management'));
+app.use(express.static(__dirname + '/dist/WeatherApp'));
 
 app.get('/*', function(req,res) {
         
-    res.sendFile(path.join(__dirname+'/dist/Email-Management/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/WeatherApp/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
